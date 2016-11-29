84bfdf92440953f20a547a2c23cce5b9b437db42
Lukas Berk <lberk@redhat.com>
2016-11-29 08:57:44 -0500

    fix capitalization

--- src/main/java/org/openshift/JaxrsConfig.java
+++ src/main/java/org/openshift/JaxrsConfig.java
@@ -4,5 +4,5 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api")
-public class jaxrsConfig extends Application{
+public class JaxrsConfig extends Application{
 }
